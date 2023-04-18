f335afdc56c6046254b796f546ab94a5f8a687b9
rmax1024 <7482819+rmax1024@users.noreply.github.com>
2023-04-18 17:15:53 -0400

    Switch to Azure Bus

---
+++ src/bicep/deploy.ps1
@@ -0,0 +1,38 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]
+    [string] $ResourcePrefix,
+
+    [Parameter(Mandatory=$true)]
+    [string] $Location = "eastus"
+)
+
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version 4.0
+$here = Split-Path -Parent $PSCommandPath
+
+if (!(Get-AzContext)) {
+    Write-Host "Login to Azure PowerShell before continuing"
+    Connect-AzAccount
+}
+
+Get-AzContext | Format-List | Out-String | Write-Host
+Read-Host "Press <RETURN> to confirm deployment into the above Azure subscription, or <CTRL-C> to cancel"
+
+$timestamp = Get-Date -f yyyyMMddTHHmmssZ
+$armParams = @{
+    location = $Location
+    prefix = $ResourcePrefix
+}
+$res = New-AzResourceGroupDeployment -Name "deploy-aind-ep01-$timestamp" `
+                                     -ResourceGroupName mrusaev `
+                                     -TemplateFile $here/main.bicep `
+                                     -TemplateParameterObject $armParams `
+                                     -Location $armParams.location `
+                                     -Verbose
+
+Write-Host "`nARM provisioning completed successfully"
+
+$tenantFqdn = Get-AzTenant | ? { $_.Id -eq (Get-AzContext).Tenant.Id } | Select -ExpandProperty Domains | Select -First 1
+Write-Host "`nPortal Link: https://portal.azure.com/#@$tenantFqdn/resource/subscriptions/$((Get-AzContext).Subscription.Id)/resourceGroups/mrusaev/overview"
+Write-Host "`nService Bus Connection String: $($res.Outputs.servicebus_connection_string.Value)"
---
+++ src/bicep/main.bicep
@@ -0,0 +1,19 @@
+﻿
+@description('The target Azure location for all resources')
+param location string
+@description('A string that will be prepended to all resource names')
+param prefix string
+
+var rgName = 'mrusaev'
+var serviceBusNamespace = '${prefix}-aind-namespace'
+
+module components 'components.bicep' = {
+  name: 'aind-components-deploy'
+  scope: resourceGroup(rgName)
+  params: {
+    location: location
+    serviceBusNamespace: serviceBusNamespace
+  }
+}
+
+output servicebus_connection_string string = components.outputs.servicebus_connection_string
